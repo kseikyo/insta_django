a1923758db206d98d45010574d004e0b31949c52
Lucas Emanuel <29212286+kseikyo@users.noreply.github.com>
2019-05-26 22:19:22 -0300

    Added login view
    Also have setted up the static files folder , updated the settings.py, and added template folders inside instagram and users folder.

--- src/instagram/settings.py
+++ src/instagram/settings.py
@@ -123,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'static_local')
+STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_files')
+] 
+
+MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
+MEDIA_URL = '/media/'
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user     = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # redirect('home')
+    else:
+        redirect('login_view')
