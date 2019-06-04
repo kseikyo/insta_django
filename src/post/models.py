cdf7fb627a730b16256fad816f2faf6e1fbb9e54
Lucas Sierota <>
2019-06-04 09:05:02 -0300

    Fixed the circular import from post and comment, also fixed the constraint from CustomUser where the foreign keys couldn't be null, but they wheren't created when the user was being created

--- src/post/models.py
+++ src/post/models.py
@@ -5,7 +5,7 @@ from comment.models import Comment
 class Post(models.Model):
     owner    = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title    = models.CharField(max_length=40)
-    comments = models.ForeignKey(Comment, on_delete=models.SET_NULL)
+    com = models.ForeignKey(Comment, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- src/users/models.py
+++ src/users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to='images/', default='/static/images/default_profile.jpeg')
-    followers       = models.ForeignKey('self', on_delete=models.CASCADE, related_name='users_followers', default=0)
-    following       = models.ForeignKey('self', on_delete=models.CASCADE, related_name='users_following', default=0)
+    followers       = models.ForeignKey('self', on_delete=models.CASCADE, related_name='users_followers', null=True)
+    following       = models.ForeignKey('self', on_delete=models.CASCADE, related_name='users_following', null=True)
     email           = models.EmailField(unique=True, null=False, blank=False)
     full_name       = models.CharField(max_length=120, null=False, blank=False)
     username        = models.CharField(max_length=110, unique=True, null=False, blank=False)
